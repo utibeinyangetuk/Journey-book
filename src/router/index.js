476b753e7c1ea5c947972149692e4c5b085a113d
utibe inyangetuk <inyangutibz@gmail.com>
2022-08-01 17:39:29 +0100

    dynamic title

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,19 @@ const routes = [
 	{
 		path: '/',
 		component: Home,
+		meta: { title: 'Journey book' },
 	},
 ]
 
 const router = createRouter({
 	history: createWebHistory(process.env.BASE_URL),
 	routes,
+})
+
+// dynamic title
+router.beforeEach((to, from, next) => {
+	document.title = `${to.meta.title}`
+	next()
 })
 
 export default router
