767399a5712904ba45fb4d5724550f90e435c52e
utibe inyangetuk <inyangutibz@gmail.com>
2022-08-01 16:24:54 +0100

    i dey learn new things dey go

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,37 @@
 <template>
-	<div>
-		<h2>hello there, brillz here</h2>
+	<div class="container">
+		<div class="greeting">
+			<h1>{{ greeting }}, <input type="text" placeholder="your name" v-model="user" /></h1>
+		</div>
+		<div class="actions">
+			<button>create a new note</button>
+			<button>view existing notes</button>
+		</div>
 	</div>
 </template>
+
+<script setup>
+	import { ref, watch, onmounted } from 'vue'
+
+	// variables
+	const user = ref('')
+	const greeting = ref('')
+
+	// get hours function block
+	const time0fday = new Date().getHours()
+	if (time0fday >= 0 && time0fday <= 11) {
+		greeting.value = 'Good morning'
+	} else if (time0fday >= 12 && time0fday <= 17) {
+		greeting.value = 'Good afternoon'
+	} else {
+		greeting.value = 'Good evening'
+	}
+</script>
+
+<style scoped>
+	.container {
+		border: 1px solid;
+		width: fit-content;
+		padding: 10px;
+	}
+</style>
